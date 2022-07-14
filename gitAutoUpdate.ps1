d748f7db91e7d74487ab2e7a8bc0744e2fd1be51
Cole Herman <cole.herman370@outlook.com>
2022-07-13 21:17:37 -0500

    Adding update script

--- gitAutoUpdate.ps1
+++ gitAutoUpdate.ps1
@@ -1,6 +1,5 @@
 #change your github location
 $github_directory = "C:\Users\coleh\Documents\Business\driessenTV"
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 Get-ChildItem  $github_directory | ForEach-Object {
     if($_.Attributes -eq "Directory")
